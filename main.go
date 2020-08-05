9b42ab5361c2285f85abcba44c49ae2f04ed9e59
IKulimanov <ivan.kulimanov@gmail.com>
2020-08-06 02:11:51 +0300

    start http server

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+var (
+	port = "8080"
+)
+
+func main() {
+
+	router := mux.NewRouter()
+	router.Use()
+
+	fmt.Printf("Start...")
+	//начало работы сервера
+	err := http.ListenAndServe(":"+port, router)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
